615fbccb7e25760eb67813b8795125abc23f49d2
ajha417 <ajha417@rku.ac.in>
2024-02-18 01:27:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './customer/components/Navigation/Navigation';
+import HomePage from './customer/page/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Navigation/>
+      <div>
+        <HomePage/>
+      </div>
     </div>
   );
 }
---
+++ src/customer/page/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import MainCarousel from "../components/HomeCarousel/MainCarousel";
+
+const HomePage = () => {
+  return (
+    <div>
+      <div>
+        <MainCarousel/>
+      </div>
+      <div>Other sections</div>
+    </div>
+  );
+};
+
+export default HomePage;
